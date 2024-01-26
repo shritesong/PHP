9b15820a94a332b0378f46c970d2d10ddcf9a942
shritesong <bird136@gmail.com>
2024-01-26 16:09:14 +0800

    complete

--- pokerv3.php
+++ pokerv3.php
@@ -1,10 +1,14 @@
 <?php
     //1.洗牌
-    $poker = [0,1,2,3,4,5];
-    $point = '';
+    $poker = [4,5,6,7,8,9];
     for($i = 0;$i < 6 ;$i++){
-        $temp = rand(0,5);
-        $temp = $poker[6-$i]
+        $last = 5-$i; //index
+        $select = rand(0,5-$i); //選取交換值
+        $temp = $poker[$last]; //將最後一個值存放到temp
+        $poker[$last] = $poker[$select]; //將交換值放到最後一個
+        $poker[$select] = $temp; //將最後一個值放入選擇的index
+        echo "{$poker[$last]}";
+
 
     }
     //2.發牌
